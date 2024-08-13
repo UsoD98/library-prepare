4c12ef303ff7251b82f077f09549befe03a685fa
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-14 08:48:22 +0900

    FEAT: DB와 통신하는 객체(DAO) 생성, @Autowired 어노테이션으로 컨트롤러에서 의존 객체 자동 주입

---
+++ src/main/java/com/company/hello/member/MemberDao.java
@@ -0,0 +1,46 @@
+package com.company.hello.member;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+@Log4j2
+@Component
+public class MemberDao {
+
+    private Map<String, MemberVo> memberDB = new HashMap<>();
+
+    public void insertMember(MemberVo memberVo) {
+        log.info("[MemberDao] insertMember()");
+
+        log.info("m_id: " + memberVo.getM_id());
+        log.info("m_pw: " + memberVo.getM_pw());
+        log.info("m_mail: " + memberVo.getM_mail());
+        log.info("m_phone: " + memberVo.getM_phone());
+
+        memberDB.put(memberVo.getM_id(), memberVo);
+        printAllMember();   // 모든 회원 정보 출력
+    }
+
+    private void printAllMember() {
+        log.info("[MemberDao] printAllMember()");
+
+        Set<String> keys = memberDB.keySet();
+        Iterator<String> iterator = keys.iterator();
+
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            MemberVo memberVo = memberDB.get(key);
+
+            log.info("m_id: " + memberVo.getM_id());
+            log.info("m_pw: " + memberVo.getM_pw());
+            log.info("m_mail: " + memberVo.getM_mail());
+            log.info("m_phone: " + memberVo.getM_phone());
+        }
+    }
+
+}
--- src/main/java/com/company/hello/member/MemberService.java
+++ src/main/java/com/company/hello/member/MemberService.java
@@ -1,11 +1,15 @@
 package com.company.hello.member;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Log4j2
 @Service
 public class MemberService {
+
+    @Autowired
+    MemberDao memberDao;
 
     public int signUpConfirm(MemberVo memberVo) {
         log.info("[MemberService] signUpConfirm()");
@@ -14,6 +18,8 @@ public class MemberService {
         log.info("m_pw: " + memberVo.getM_pw());
         log.info("m_mail: " + memberVo.getM_mail());
         log.info("m_phone: " + memberVo.getM_phone());
+
+        memberDao.insertMember(memberVo);
 
         return 0;
     }
