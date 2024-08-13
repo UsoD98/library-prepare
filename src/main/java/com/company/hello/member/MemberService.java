26ec67aba9b96279ba6149d72e234d86100e3004
Jaewon Kim <130049758+UsoD98@users.noreply.github.com>
2024-08-13 22:37:25 +0900

    FEAT: @Service 어노테이션을 이용산 서비스 빈 객체 생성

--- src/main/java/com/company/hello/member/MemberService.java
+++ src/main/java/com/company/hello/member/MemberService.java
@@ -1,8 +1,10 @@
 package com.company.hello.member;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
 
 @Log4j2
+@Service
 public class MemberService {
 
     public int signUpConfirm(MemberVo memberVo) {
